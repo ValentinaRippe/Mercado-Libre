2246e8c53a92218121309ef2bd9fde555d9b1d1d
ValentinaRippe <lauravalentina200102@gmail.com>
2024-02-29 15:31:48 -0500

    first commit

---
+++ src/router/RouterApp.jsx
@@ -0,0 +1,11 @@
+import { Route, Routes } from "react-router-dom";
+
+const RouterApp = () => {
+  return (
+    <Routes>
+      <Route path="/" />
+    </Routes>
+  );
+};
+
+export default RouterApp;
