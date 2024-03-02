3d427036a2f272833105edaeb1a8b6b0ebc6db92
ValentinaRippe <lauravalentina200102@gmail.com>
2024-03-02 14:03:27 -0500

    Finish home

--- src/routes/routes.jsx
+++ src/routes/routes.jsx
@@ -1,8 +1,8 @@
 import { createBrowserRouter } from "react-router-dom";
 
-import { Detail } from "../pages/Detail/Detail";
-import { Home } from "../pages/Home/Home";
-import { Search } from "../pages/Search/Search";
+import { Detail } from "../pages/Detail/Detail.jsx";
+import { Home } from "../pages/Home/Home.jsx";
+import { Search } from "../pages/Search/Search.jsx";
 import { Root } from "./root.jsx";
 
 export const router = createBrowserRouter([
